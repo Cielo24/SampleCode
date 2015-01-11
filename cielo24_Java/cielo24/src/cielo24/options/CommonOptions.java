42ec05f3b48a66975b6b5e1dc75ac90b6df41926
Jack <z_chiz@live.com>
2015-01-10 23:35:58 -0800

    Java (in progress)

--- cielo24_Java/cielo24/src/cielo24/options/CommonOptions.java
+++ cielo24_Java/cielo24/src/cielo24/options/CommonOptions.java
@@ -8,14 +8,14 @@ import static cielo24.Enums.*;
 
 public abstract class CommonOptions extends BaseOptions {
 
-	@QueryName("characters_per_caption_line")
-	public Integer charactersPerCaptionLine = null;
 	@QueryName("elementlist_version")
 	public NanoDate elementListVersion = null;
 	@QueryName("emit_speaker_change_token_as")
 	public String speakerChangeToken = null;
 	@QueryName("mask_profanity")
 	public Boolean maskProfanity = null;
+	@QueryName("remove_disfluencies")
+    public Boolean removeDisfluencies = null;
 	@QueryName("remove_sounds_list")
 	public ArrayList<Tag> removeSoundsList = null;
 	@QueryName("remove_sound_references")
@@ -24,6 +24,4 @@ public abstract class CommonOptions extends BaseOptions {
 	public Boolean replaceSlang = null;
 	@QueryName("sound_boundaries")
 	public char[] soundBoundaries = null;
-
-	public CommonOptions() {}
 }
---
+++ cielo24_Java/cielo24/src/cielo24/options/JobListOptions.java
@@ -0,0 +1,21 @@
+package cielo24.options;
+
+import cielo24.utils.NanoDate;
+import cielo24.Enums.*;
+
+public class JobListOptions extends BaseOptions {
+    public NanoDate CreationDateFrom = null;
+    public NanoDate CreationDateTo = null;
+    public NanoDate StartDateFrom = null;
+    public NanoDate StartDateTo = null;
+    public NanoDate DueDateFrom = null;
+    public NanoDate DueDateTo = null;
+    public NanoDate CompleteDateFrom = null;
+    public NanoDate CompleteDateTo = null;
+    public JobStatus JobStatus = null;
+    public Fidelity Fidelity = null;
+    public Priority Priority = null;
+    public Integer TurnaroundTimeHoursFrom = null;
+    public Integer TurnaroundTimeHoursTo = null;
+    public String username = null;
+}
