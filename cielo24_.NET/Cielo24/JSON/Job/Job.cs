ad114fed1c6e9cb491e9453898a54c2a28706f53
Jack <z_chiz@live.com>
2015-02-02 12:29:07 -0800

    EN-2750: .NET Job and JobInfo updated

--- cielo24_.NET/Cielo24/JSON/Job/Job.cs
+++ cielo24_.NET/Cielo24/JSON/Job/Job.cs
@@ -12,25 +12,47 @@ namespace Cielo24.JSON.Job
         public Guid JobId { get; set; }
         [JsonProperty("JobName")]
         public string JobName { get; set; }
-        [JsonProperty("JobStatus")]
-        public JobStatus? JobStatus { get; set; }
+        [JsonProperty("MediaLengthSeconds")]
+        public int? MediaLengthSeconds { get; set; }
+        [JsonProperty("ExternalID")]
+        public String ExternalID { get; set; }
         [JsonProperty("Priority")]
         public Priority? Priority { get; set; }
         [JsonProperty("Fidelity")]
         public Fidelity? Fidelity { get; set; }
-        [JsonProperty("JobLanguage")]
-        public string JobLanguage { get; set; }
-        [JsonProperty("TargetLanguage")]
-        public string TargetLanguage { get; set; }
-        [JsonProperty("CreationTime")]
-        public DateTime? CreationTime { get; set; }
-        [JsonProperty("DueDate")]
-        public DateTime? DueDate { get; set; }
         [JsonProperty("TurnaroundTimeHours")]
         public int? TurnaroundTimeHours { get; set; }
+        [JsonProperty("JobStatus")]
+        public JobStatus? JobStatus { get; set; }
+        [JsonProperty("SourceLanguage")]
+        public string SourceLanguage { get; set; }
+        [JsonProperty("TargetLanguage")]
+        public string TargetLanguage { get; set; }
+        [JsonProperty("CreationDate")]
+        public DateTime? CreationDate { get; set; }
+        [JsonProperty("StartDate")]
+        public DateTime? StartDate { get; set; }
+        [JsonProperty("DueDate")]
+        public DateTime? DueDate { get; set; }
+        [JsonProperty("CompletedDate")]
+        public DateTime? CompletedDate { get; set; }
+        [JsonProperty("ReturnTargets")]
+        public Dictionary<String, String> ReturnTargets { get; set; }
+        [JsonProperty("Options")]
+        public Dictionary<String, String> Options { get; set; }
+
+        // BACKWARD COMPATABILITY //
+        [JsonProperty("JobLanguage")]
+        [Obsolete("This property is obsolete; use SourceLanguage property instead.")]
+        public string JobLanguage { get; set; }
+        [JsonProperty("CreationTime")]
+        [Obsolete("This property is obsolete; use CreationDate property instead.")]
+        public DateTime? CreationTime { get; set; }
         [JsonProperty("StartTime")]
+        [Obsolete("This property is obsolete; use StartDate property instead.")]
         public DateTime? StartTime { get; set; }
         [JsonProperty("CompletedTime")]
+        [Obsolete("This property is obsolete; use CompletedDate property instead.")]
         public DateTime? CompletedTime { get; set; }
     }
 }
--- cielo24_.NET/Cielo24/JSON/Job/JobInfo.cs
+++ cielo24_.NET/Cielo24/JSON/Job/JobInfo.cs
@@ -12,9 +12,42 @@ namespace Cielo24.JSON.Job
         public Guid JobId { get; set; }
         [JsonProperty("JobName")]
         public string JobName { get; set; }
+        [JsonProperty("MediaLengthSeconds")]
+        public int? MediaLengthSeconds { get; set; }
+        [JsonProperty("ExternalID")]
+        public String ExternalID { get; set; }
+        [JsonProperty("Priority")]
+        public Priority? Priority { get; set; }
+        [JsonProperty("Fidelity")]
+        public Fidelity? Fidelity { get; set; }
+        [JsonProperty("TurnaroundTimeHours")]
+        public int? TurnaroundTimeHours { get; set; }
+        [JsonProperty("JobStatus")]
+        public JobStatus? JobStatus { get; set; }
+        [JsonProperty("SourceLanguage")]
+        public string SourceLanguage { get; set; }
+        [JsonProperty("TargetLanguage")]
+        public string TargetLanguage { get; set; }
+        [JsonProperty("CreationDate")]
+        public DateTime? CreationDate { get; set; }
+        [JsonProperty("StartDate")]
+        public DateTime? StartDate { get; set; }
+        [JsonProperty("DueDate")]
+        public DateTime? DueDate { get; set; }
+        [JsonProperty("CompletedDate")]
+        public DateTime? CompletedDate { get; set; }
+        [JsonProperty("ReturnTargets")]
+        public Dictionary<String, String> ReturnTargets { get; set; }
+        [JsonProperty("Options")]
+        public Dictionary<String, String> Options { get; set; }
+
+        // BACKWARD COMPATABILITY //
         [JsonProperty("Language")]
+        [Obsolete("This property is obsolete; use SourceLanguage property instead.")]
         public string Language { get; set; }
         [JsonProperty("Tasks")]
+        [Obsolete("This property is obsolete and should not be used.")]
         public List<Task> Tasks { get; set; }
+        // TaskCompletion time never returned ?
     }
 }
