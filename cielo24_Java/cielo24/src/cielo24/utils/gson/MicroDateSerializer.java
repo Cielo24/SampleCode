eee4c39b4209c196cb3b72b8642f362a87f119f9
Jack <z_chiz@live.com>
2015-03-19 14:47:36 -0700

    EN-3198: fixed invalid types in serializer

--- cielo24_Java/cielo24/src/cielo24/utils/gson/MicroDateSerializer.java
+++ cielo24_Java/cielo24/src/cielo24/utils/gson/MicroDateSerializer.java
@@ -1,17 +1,17 @@
 package cielo24.utils.gson;
 
 import java.lang.reflect.Type;
-import java.util.Date;
 
+import cielo24.utils.MicroDate;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
-public class MicroDateSerializer implements JsonSerializer<Date> {
+public class MicroDateSerializer implements JsonSerializer<MicroDate> {
 
     @Override
-    public JsonElement serialize(Date date, Type type, JsonSerializationContext context) {
+    public JsonElement serialize(MicroDate date, Type type, JsonSerializationContext context) {
         return new JsonPrimitive(date.toString());
     }
 }
